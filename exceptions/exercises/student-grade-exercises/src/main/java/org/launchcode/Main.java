63321bf485ab9299ab19e1fce0b1f381066664bc
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-11-19 13:58:13 -0600

    Exception : chapter example and Exercise completed

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
-
+        System.out.println(Divide(2,2));
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
@@ -13,15 +13,44 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for(HashMap.Entry<String,String> entry : studentFiles.entrySet()){
+            int point = 0;
+            point = CheckFileExtension(entry.getValue());
+            System.out.println(entry.getKey()+":"+point+"points");
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static double Divide(int x, int y)
     {
         // Write code here!
+        double div=0;
+        try{
+            div = x/y;
+        }catch(ArithmeticException arithmeticException){
+            System.out.println("Error due to:"+ arithmeticException.getMessage());
+            arithmeticException.printStackTrace();
+        }
+        return div;
     }
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        int points = 0;
+        if(fileName == null || fileName.isEmpty()) {
+                points = -1;
+                try {
+                     throw new NoFileException("No File has been uploaded");
+                }catch(NoFileException e){
+                    e.printStackTrace();
+                }
+
+        }else if(fileName.endsWith(".java")){
+                points = 1;
+        }else{
+            points = 0;
+        }
+
+        return points;
     }
 }
